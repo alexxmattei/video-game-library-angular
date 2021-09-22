e7ccdabec0d6de7e3d7947a831383b138295b8de
alexandru matei <alexxmattei@gmail.com>
2021-09-23 01:46:52 +0300

    updated api requests and method data business logic

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
   gameRating = 0;
-  constructor() { }
+  gameId!: string;
+  game!: Game;
+  routeSub: Subscription = new Subscription;
+  gameSub: Subscription = new Subscription;
+
+  constructor(private activatedRoute: ActivatedRoute, private httpService: HttpService) { }
 
   ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['id'];
+      this.getGameDetails(this.gameId);
+    });
+  }
+
+  getGameDetails(id: string) {
+    this.gameSub = this.httpService.getGameDetails(id).subscribe((gameResp: Game) => {
+      this.game = gameResp;
+      setTimeout(() => {
+        this.gameRating = this.game.metacritic
+      }, 1000);
+    });
   }
 
   getColor(value: number): string {
