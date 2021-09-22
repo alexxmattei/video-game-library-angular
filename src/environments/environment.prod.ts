43f0e8788f5943eeb6b7e51447b87094c38a48a6
alexandru matei <alexxmattei@gmail.com>
2021-09-22 21:29:19 +0300

    updated .gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true, 
-  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com',
-  API_KEY: 'b7bde76e03msh3d774587b5e7abep12f0f3jsn169d35326bff',
-  API_HOST: 'rawg-video-games-database.p.rapidapi.com'
-};
